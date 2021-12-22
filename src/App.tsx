ce2216f2e7ee40d9b8867a3ea150376589b66e78
h4i4nhnc <haianhnc@mirabo.com.vn>
2021-12-22 11:25:07 +0700

    init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import 'bulma/css/bulma.min.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <div className="buttons">
+        <button className="button is-primary">Primary</button>
+        <button className="button is-link">Link</button>
+        <button className="button is-info">Info</button>
+        <button className="button is-success">Success</button>
+        <button className="button is-warning">Warning</button>
+        <button className="button is-danger">Danger</button>
+        <button className="button is-black">Black</button>
+        <button className="button is-white">White</button>
+        <button className="button is-dark">Dark</button>
+        <button className="button is-light">Light</button>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
